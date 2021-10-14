589c6b4a7a425943c7ce6f5713e681ffc8d08bd8
ainneo <ainneo@gmail.com>
2021-10-14 16:08:05 -0700

    add notes

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -7,19 +7,25 @@ require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
 export default function App() {
-  const [data, setData] = useState([]);
-  const [q, setQ] = useState('');
+
+  //state
+  const [data, setData] = useState([]);//copy of stored data state
+  const [q, setQ] = useState('');//search bar input value state
   const [searchColumns, setSearchColumns] = useState([
     'name',
     'birth_year',
-  ]);
+  ]); //checkbox values - initail state is set
 
+  // api data fetch
   useEffect(() => {
     fetch('https://swapi.dev/api/people')
       .then((response) => response.json())
       .then((json) => setData(json.results));
-  }, []);
+  }, []); //only gets called once
 
+  //search function, row = data
+  //filters through data state and returns filtered data results based on search input value and checkbox values
+  //search function passes in the data and filters through each item in the array, and returns the filtered results
   function search(rows) {
     return rows.filter((row) =>
       searchColumns.some(
@@ -32,15 +38,23 @@ export default function App() {
     );
   }
 
+  //init column checkbox state
+  //console.log(data) //[{}, {}, {}]
+  //get all of the data object keys
+  //since all objects have the same keys, we can use the first object to get the keys
   const columns = data[0] && Object.keys(data[0]);
+
   return (
     <div>
       <div>
+        {/* search box */}
         <input
           type='text'
           value={q}
           onChange={(e) => setQ(e.target.value)}
         />
+
+        {/* checkbox */}
         {columns &&
           columns.map((column) => (
             <label>
@@ -62,7 +76,11 @@ export default function App() {
       </div>
       <div>
         <Datatable data={search(data)} />
+          {/* passing in data state into datatable */}
+          {/* search function is automatically invoked on every render and passes in the initial data state */}
+          {/* search updates based on the checked and value inputs*/}
       </div>
     </div>
   );
 }
+
--- src/datatable/index.jsx
+++ src/datatable/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
 export default function Datatable({ data }) {
+  // console.log(data);//testing
+
+  //get all the keys of object 0 and display them
   const columns = data[0] && Object.keys(data[0]);
+
   return (
     <table cellPadding={0} cellSpacing={0}>
+      {/* mapping out table keys */}
       <thead>
         <tr>
           {data[0] && columns.map((heading) => <th>{heading}</th>)}
         </tr>
       </thead>
+      {/* mapping out table values */}
       <tbody>
         {data.map((row) => (
           <tr>
